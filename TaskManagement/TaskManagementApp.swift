8cad63a1faed1b75ca2780b0ec6d657ef5fa10df
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-04-13 00:31:18 +0200

    first commit: Load initial files

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Marcin Jędrzejak on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
